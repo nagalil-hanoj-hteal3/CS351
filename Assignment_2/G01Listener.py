cd81b2c83949e7c617600ffc9e1db56f3bf19fd6
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-05-04 15:35:17 -0700

    Create G01Listener.py

---
+++ Assignment_2/G01Listener.py
@@ -0,0 +1,57 @@
+# Generated from G01.g4 by ANTLR 4.9
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .G01Parser import G01Parser
+else:
+    from G01Parser import G01Parser
+
+# This class defines a complete listener for a parse tree produced by G01Parser.
+class G01Listener(ParseTreeListener):
+
+    # Enter a parse tree produced by G01Parser#start.
+    def enterStart(self, ctx:G01Parser.StartContext):
+        pass
+
+    # Exit a parse tree produced by G01Parser#start.
+    def exitStart(self, ctx:G01Parser.StartContext):
+        pass
+
+
+    # Enter a parse tree produced by G01Parser#oneDrawExprUsingG01.
+    def enterOneDrawExprUsingG01(self, ctx:G01Parser.OneDrawExprUsingG01Context):
+        pass
+
+    # Exit a parse tree produced by G01Parser#oneDrawExprUsingG01.
+    def exitOneDrawExprUsingG01(self, ctx:G01Parser.OneDrawExprUsingG01Context):
+        pass
+
+
+    # Enter a parse tree produced by G01Parser#anotherDrawExprUsingG28.
+    def enterAnotherDrawExprUsingG28(self, ctx:G01Parser.AnotherDrawExprUsingG28Context):
+        pass
+
+    # Exit a parse tree produced by G01Parser#anotherDrawExprUsingG28.
+    def exitAnotherDrawExprUsingG28(self, ctx:G01Parser.AnotherDrawExprUsingG28Context):
+        pass
+
+
+    # Enter a parse tree produced by G01Parser#lastDrawExprUsingF.
+    def enterLastDrawExprUsingF(self, ctx:G01Parser.LastDrawExprUsingFContext):
+        pass
+
+    # Exit a parse tree produced by G01Parser#lastDrawExprUsingF.
+    def exitLastDrawExprUsingF(self, ctx:G01Parser.LastDrawExprUsingFContext):
+        pass
+
+
+    # Enter a parse tree produced by G01Parser#printForAllGrammarCodes.
+    def enterPrintForAllGrammarCodes(self, ctx:G01Parser.PrintForAllGrammarCodesContext):
+        pass
+
+    # Exit a parse tree produced by G01Parser#printForAllGrammarCodes.
+    def exitPrintForAllGrammarCodes(self, ctx:G01Parser.PrintForAllGrammarCodesContext):
+        pass
+
+
+
+del G01Parser
